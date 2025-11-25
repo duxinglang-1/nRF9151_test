8f74bc29073d149a238293e9314b5b1ac69b4779
Biao.X <3447782@qq.com>
2025-11-25 14:25:33 +0800

    添加新的传感器模块。

---
+++ N2_NCS2.7.0_TEST/src/ble/ble.c
@@ -0,0 +1,16 @@
+/****************************************Copyright (c)************************************************
+** File name:			    BLE.c
+** Last modified Date:          
+** Last Version:		   
+** Descriptions:		   	使用的ncs版本-2.7.0		
+** Created by:				谢彪
+** Created date:			2025-11-25
+** Version:			    	1.0
+** Descriptions:			BLE事件处理源文件
+******************************************************************************************************/
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+
---
+++ N2_NCS2.7.0_TEST/src/ble/ble.h
@@ -0,0 +1,15 @@
+/****************************************Copyright (c)************************************************
+** File name:			    BLE.c
+** Last modified Date:          
+** Last Version:		   
+** Descriptions:		   	使用的ncs版本-2.7.0		
+** Created by:				谢彪
+** Created date:			2025-11-25
+** Version:			    	1.0
+** Descriptions:			BLE事件处理源文件
+******************************************************************************************************/
+#ifndef __BLE_H__
+#define __BLE_H__
+
+
+#endif/*__BLE_H__*/

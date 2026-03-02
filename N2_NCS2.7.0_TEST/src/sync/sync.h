9785b70feec1e02dce8a406db7944dce3d8228f8
Biao.X <3447782@qq.com>
2026-03-02 11:26:16 +0800

    去掉多余的宏定义。

--- N2_NCS2.7.0_TEST/src/sync/sync.h
+++ N2_NCS2.7.0_TEST/src/sync/sync.h
@@ -17,22 +17,14 @@ extern "C" {
 #endif
 
 //synchronism data
-#define SYNC_ICON_W					135
-#define SYNC_ICON_H					156
+#define SYNC_ICON_W					220
+#define SYNC_ICON_H					254
 #define SYNC_ICON_X					((LCD_WIDTH-SYNC_ICON_W)/2)
-#define SYNC_ICON_Y					37
-#define SYNC_FINISH_ICON_W			135
-#define SYNC_FINISH_ICON_H			156
-#define SYNC_FINISH_ICON_X			((LCD_WIDTH-SYNC_ICON_W)/2)
-#define SYNC_FINISH_ICON_Y			16
-#define SYNC_STR_W					92
-#define SYNC_STR_H					45
-#define SYNC_STR_X					((LCD_WIDTH-SYNC_STR_W)/2)
-#define SYNC_STR_Y					133
-#define SYNC_RUNNING_ANI_W			81
-#define SYNC_RUNNING_ANI_H			13
+#define SYNC_ICON_Y					79
+#define SYNC_RUNNING_ANI_W			132
+#define SYNC_RUNNING_ANI_H			22
 #define SYNC_RUNNING_ANI_X			((LCD_WIDTH-SYNC_RUNNING_ANI_W)/2)
-#define SYNC_RUNNING_ANI_Y			212
+#define SYNC_RUNNING_ANI_Y			378
 
 typedef enum
 {
